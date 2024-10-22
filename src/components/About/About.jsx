60369baa2fddf266c48f24ae6c63c2db7e9196af
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-10-21 23:25:20 -0400

    Added About page and styled accordingly

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import authorImage from "../../assets/authorPlaceholder.png";
+import authorImage from "../../assets/authorPlaceholder.avif";
 import "./About.css";
+
 function About() {
   return (
     <section className="about">
       <div className="about__content">
         <img src={authorImage} alt="author" className="about__image" />
-        <h2 className="about__title">About the author</h2>
+        <div className="about__text">
+          <h2 className="about__title">About the author</h2>
+          <p className="about__paragraph">
+            This block describes the author of the site, when he was born, where
+            he was born, and where he studied.
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,23 +2,23 @@ import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import NotFound from "../NotFound/NotFound";
+import About from "../About/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="page">
-        <div className="page__content">
-          <div className="main-section">
-            <div className="main-section__content">
-              <Header />
-              <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </div>
+        <div className="main-section">
+          <div className="main-section__content">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Main />} />
+            </Routes>
           </div>
         </div>
+        {/* <NotFound /> */}
+        <About />
       </div>
     </BrowserRouter>
   );
