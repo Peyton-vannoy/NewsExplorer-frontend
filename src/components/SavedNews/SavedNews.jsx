d68625dedacfd68cf4de5472f66f925bd8469c0d
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-11-22 13:03:52 -0500

    saved news saved article section added

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,9 +1,48 @@
 import "./SavedNews.css";
+import NewsCardList from "../NewsCardList/NewsCardList";
 
 function SavedNews() {
+  const savedArticles = [
+    {
+      title: "Title",
+      description: "Description",
+      publishedAt: "2024-01-01",
+      source: "Source",
+      urlToImage: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Title",
+      description: "Description",
+      publishedAt: "2024-01-01",
+      source: "Source",
+      urlToImage: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Title",
+      description: "Description",
+      publishedAt: "2024-01-01",
+      source: "Source",
+      urlToImage: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Title",
+      description: "Description",
+      publishedAt: "2024-01-01",
+      source: "Source",
+      urlToImage: "https://via.placeholder.com/150",
+    },
+    {
+      title: "Title",
+      description: "Description",
+      publishedAt: "2024-01-01",
+      source: "Source",
+      urlToImage: "https://via.placeholder.com/150",
+    },
+  ];
+
   return (
     <div className="saved__news">
-      <div className="saved__news-header">
+      <section className="saved__news-header">
         <p className="saved__news-label">Saved articles</p>
         <h1 className="saved__news-title">Peyton, you have 5 saved articles</h1>
         <p className="saved__news-keywords">
@@ -12,7 +51,10 @@ function SavedNews() {
             Nature, Yellowstone, and 2 other
           </span>
         </p>
-      </div>
+      </section>
+      <section className="saved__news-cards">
+        <NewsCardList articles={savedArticles} />
+      </section>
     </div>
   );
 }
