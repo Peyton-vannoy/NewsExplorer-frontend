8bae960e7fca61cdb761299f4afedc4c592225d9
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-11-25 20:47:27 -0500

    mobile breakpoints and styling pt 2

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -13,16 +13,18 @@ function SearchForm({ onSearch }) {
 
   return (
     <form className="search-form" onSubmit={handleSubmit}>
-      <input
-        className="search-form__input"
-        type="text"
-        placeholder="Enter topic"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-      />
-      <button className="search-form__button" type="submit">
-        Search
-      </button>
+      <div className="search-form__input-container">
+        <input
+          className="search-form__input"
+          type="text"
+          placeholder="Enter topic"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className="search-form__button" type="submit">
+          Search
+        </button>
+      </div>
     </form>
   );
 }
