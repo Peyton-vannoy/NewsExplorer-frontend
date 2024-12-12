fe276a6473d7148c3bd4bc9fb1b4e19d214ddfb9
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-12-12 01:17:12 -0500

    added apikey into production/ says im missing apikey

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,8 @@ export const BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://nomoreparties.co/news/v2/everything"
     : "https://newsapi.org/v2/everything";
+
+export const API_KEY =
+  process.env.NODE_ENV === "production"
+    ? "00000000000000000000000000000000"
+    : import.meta.env.VITE_NEWS_API_KEY;
