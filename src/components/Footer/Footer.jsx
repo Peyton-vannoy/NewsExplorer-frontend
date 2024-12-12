74a0967e16836c21d26fc94ed9a4f68913cbbbe4
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-12-12 03:39:11 -0500

    footer nave mobile styled and refactored

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,34 +6,45 @@ function Footer() {
     <footer className="footer">
       <div className="footer__content">
         <p className="footer__copyright">
-          © 2024 Supersite, Powered by News API
+          &copy;2024 Supersite, Powered by News API
         </p>
         <nav className="footer__nav">
-          <Link to="/" className="footer__link">
-            Home
-          </Link>
-          <a
-            href="https://tripleten.com"
-            target="_blank"
-            className="footer__link"
-          >
-            TripleTen
-          </a>
-
-          <a
-            href="https://github.com/Peyton-vannoy"
-            target="_blank"
-            className="footer__icon"
-          >
-            <i className="fab fa-github"></i>
-          </a>
-          <a
-            href="https://www.facebook.com/peyton.vannoy19"
-            target="_blank"
-            className="footer__icon"
-          >
-            <i className="fab fa-facebook"></i>
-          </a>
+          <ul className="footer__links">
+            <li>
+              <Link to="/" className="footer__link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <a
+                href="https://tripleten.com"
+                target="_blank"
+                className="footer__link"
+              >
+                TripleTen
+              </a>
+            </li>
+          </ul>
+          <ul className="footer__icons">
+            <li>
+              <a
+                href="https://github.com/Peyton-vannoy"
+                target="_blank"
+                className="footer__icon"
+              >
+                <i className="fab fa-github"></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/peyton.vannoy19"
+                target="_blank"
+                className="footer__icon"
+              >
+                <i className="fab fa-facebook"></i>
+              </a>
+            </li>
+          </ul>
         </nav>
       </div>
     </footer>
